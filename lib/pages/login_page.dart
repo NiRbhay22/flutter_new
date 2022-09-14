80d0abb3326ca10b8104ec35e741499274648b86
NiRbhay22 <niknirbhay@gmail.com>
2022-09-15 00:45:34 +0530

    day 3 commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+          child: Text(
+        "Login page",
+        style: TextStyle(
+          fontSize: 40,
+          color: Colors.red,
+          fontWeight: FontWeight.bold,
+        ),
+      )),
+    );
+  }
+}
